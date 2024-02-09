07b823bf31b512365e58470e6bfd1f513d7cf551
Goodluck Japhet <goodluckjaphet7@gmail.com>
2024-02-10 00:49:34 +0300

    first commit

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,33 @@
+import {FaSearch} from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+export default function Header() {
+  return (
+    <header className="bg-blue-100 shadow-md">
+    <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+        <Link to="/">
+        <img src='https://github.com/GoldenCicada1/rgpapp-react/blob/main/public/logo-blue.png?raw=true'
+        alt='logo'
+        className='h-5 sm:h-7 cursor-pointer' />
+        </Link>
+        
+    <form className="bg-blue-50 p-2 rounded-lg flex items-center">
+        <input type="text" placeholder="Search" 
+        className="bg-transparent focus:outline-none w-24 sm:w-64" />
+        <FaSearch className="text-blue-400"/>
+    </form>
+    <ul className="flex gap-4">
+        <Link to="/">
+        <li className="hidden sm:inline text-blue-400 hover:underline">Home</li>
+        </Link>
+        <Link to="/about">
+        <li className="hidden sm:inline text-blue-400 hover:underline">About</li>
+        </Link>
+        <Link to="/sign-in">
+        <li className="text-blue-400 hover:underline">Sign In</li>
+        </Link>
+    </ul>
+    </div>
+    </header>
+  )
+}

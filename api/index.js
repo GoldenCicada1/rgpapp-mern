cebb267fad4960a59fbcd2363b0fcab4c9fe0659
Goodluck Japhet <goodluckjaphet7@gmail.com>
2024-02-27 23:57:10 +0300

    Created admin Model, control and Router, Works fine

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
+import adminRouter from "./routes/admin.route.js";
 import productRouter from "./routes/product.route.js";
 import mediaRouter from "./routes/media.route.js";
 import locationRouter from "./routes/location.route.js";
@@ -46,6 +47,7 @@ app.use("/api/building", buildingRouter);
 app.use("/api/company", companyRouter);
 app.use("/api/landOwnership", landOwnershipRouter);
 app.use("/api/buildingOwnership", buildingOwnershipRouter);
+app.use("/api/admin", adminRouter);
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
